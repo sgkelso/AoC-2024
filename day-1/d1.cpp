02a8af659c398d44d70da388f1a518b9a3386d7e
Spencer Kelso <sgkelso@ncsu.edu>
2024-12-06 17:51:51 -0500

    finish day 1, p1

--- day-1/d1.cpp
+++ day-1/d1.cpp
@@ -5,21 +5,29 @@
 #include <string>
 #include "stdio.h"
 using namespace std;
-
+bool comp (int i, int j) {return (i < j);}
 
 int main(){
-    cout << "Starting read file" << endl;
+    
     FILE *fp = fopen("day1.txt","r");
     if(fp == nullptr){
         cout << "error reading file" << endl;
         exit(0);
     }
-    int one;
-    int two;
-    while(fp != EOF){
-        scanf("%d %d", &one, &two);
-    }
     vector<int> l1;
     vector<int> l2;
-
+    int one;
+    int two;
+    //parse from input file
+    while(fscanf(fp,"%d %d",&one,&two) == 2){
+        l1.push_back(one);
+        l2.push_back(two);
+    }
+    sort(l1.begin(),l1.end(), comp);
+    sort(l2.begin(),l2.end(), comp);
+    int sum = 0;
+    for(int i = 0; i < l1.size(); i++){
+        sum += abs(l1.at(i) - l2.at(i));
+    }
+    cout << sum << endl;
 }
